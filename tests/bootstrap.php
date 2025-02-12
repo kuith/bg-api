5635e20d706fece225354a6c10b9ab724e5458a3
kuith <kuithsteam@gmail.com>
2025-02-12 13:51:02 +0100

    pruebas de configuracion phpUnit
    Aún con problemas de configuración de phpUnit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,6 @@
 <?php
 
-use Doctrine\ORM\Tools\SchemaTool;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Bundle\FrameworkBundle\KernelBrowser;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
@@ -16,7 +12,7 @@ $kernel = new \App\Kernel('test', true);
 $kernel->boot();
 
 /** @var EntityManagerInterface $entityManager */
-$entityManager = $kernel->getContainer()->get(EntityManagerInterface::class);
+/**$entityManager = $kernel->getContainer()->get(EntityManagerInterface::class);*/
 $metadata = $entityManager->getMetadataFactory()->getAllMetadata();
 
 $schemaTool = new SchemaTool($entityManager);
